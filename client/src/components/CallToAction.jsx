e98adad130212fa58e4bb53422f3d6ec6330e076
Sinisterchilll <anucbs22@gmail.com>
2024-02-02 11:42:36 +0530

    first

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,24 @@
+import { Button } from 'flowbite-react';
+
+export default function CallToAction() {
+  return (
+    <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
+        <div className="flex-1 justify-center flex flex-col">
+            <h2 className='text-2xl'>
+                Want to learn more about me?
+            </h2>
+            <p className='text-gray-500 my-2'>
+                Checkout these links!
+            </p>
+            <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
+                <a href="https://www.100jsprojects.com" target='_blank' rel='noopener noreferrer'>
+                    me
+                </a>
+            </Button>
+        </div>
+        <div className="p-7 flex-1">
+            <img src="https://static.wikia.nocookie.net/faf2fa40-6c7e-4910-9b04-a0ee169a1d8a/scale-to-width-down/800" />
+        </div>
+    </div>
+  )
+}
---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjhDX2A8ziHqWQ9BB7wtdJrUM246-fcG8",
+  authDomain: "project-manager-e57ca.firebaseapp.com",
+  projectId: "project-manager-e57ca",
+  storageBucket: "project-manager-e57ca.appspot.com",
+  messagingSenderId: "89728921522",
+  appId: "1:89728921522:web:b2a62b0e02f7538a40feab"
+};
+
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
