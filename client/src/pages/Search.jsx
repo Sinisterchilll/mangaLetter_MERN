99c2d836b597c3410754083b1622042e2b3f1689
Sinisterchilll <anucbs22@gmail.com>
2024-02-11 19:48:33 +0530

    improving filter

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -131,9 +131,9 @@ export default function Search() {
               id='category'
             >
               <option value='uncategorized'>Uncategorized</option>
-              <option value='reactjs'>React.js</option>
-              <option value='nextjs'>Next.js</option>
-              <option value='javascript'>JavaScript</option>
+              <option value='reactjs'>Ongoing</option>
+              <option value='nextjs'>Completed</option>
+              <option value='javascript'>Upcoming</option>
             </Select>
           </div>
           <Button type='submit' outline gradientDuoTone='purpleToPink'>
