2ad7e3932d5eaee9c1fb25c5eaaa264c28daf301
Sinisterchilll <anucbs22@gmail.com>
2024-02-11 17:25:31 +0530

    updating about section

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,27 +4,32 @@ export default function About() {
       <div className='max-w-2xl mx-auto p-3 text-center'>
         <div>
           <h1 className='text-3xl font font-semibold text-center my-7'>
-            About Sahand' Blog
+            About Manga Mansion
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
-              Welcome to Sahand's Blog! This blog was created by Sahand Ghavidel
-              as a personal project to share his thoughts and ideas with the
-              world. Sahand is a passionate developer who loves to write about
-              technology, coding, and everything in between.
+            At Manga Mansion, we're more than just a community - we're a family
+            of passionate manga lovers, united by our love for epic battles, 
+            heartwarming stories, and unforgettable characters. 
+            From the bustling streets of Tokyo to the far reaches of the anime 
+            multiverse, our doors are open to all who dare to dream!
             </p>
 
             <p>
-              On this blog, you'll find weekly articles and tutorials on topics
-              such as web development, software engineering, and programming
-              languages. Sahand is always learning and exploring new
-              technologies, so be sure to check back often for new content!
+            Experience the Magic of Manga Letter
+
+            Prepare to be whisked away on a thrilling journey with Manga Letter, 
+            our exclusive newsletter that's not just a publication - it's a portal 
+            to a world of endless possibilities! 📰✨ Dive into the latest news, 
+            exclusive interviews, and behind-the-scenes secrets straight from the 
+            heart of the manga industry. With each edition, you'll embark on an 
+            adventure that's as thrilling as the pages of your favorite manga series!
             </p>
 
             <p>
               We encourage you to leave comments on our posts and engage with
               other readers. You can like other people's comments and reply to
-              them as well. We believe that a community of learners can help
+              them as well. We believe that a community of men of culture can help
               each other grow and improve.
             </p>
           </div>
