2d06ad54ac43135cd0c8324319445d22a2cc4141
Sinisterchilll <anucbs22@gmail.com>
2024-02-11 19:21:53 +0530

    updating categories

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -103,9 +103,9 @@ export default function CreatePost() {
             }
           >
             <option value='uncategorized'>Select a category</option>
-            <option value='javascript'>JavaScript</option>
-            <option value='reactjs'>React.js</option>
-            <option value='nextjs'>Next.js</option>
+            <option value='javascript'>Ongoing</option>
+            <option value='reactjs'>Completed</option>
+            <option value='nextjs'>Upcoming</option>
           </Select>
         </div>
         <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -3,7 +3,7 @@ import CallToAction from '../components/CallToAction';
 export default function Projects() {
   return (
     <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
-      <h1 className='text-3xl font-semibold'>Merchendise</h1>
+      <h1 className='text-3xl font-semibold'>Merchandise</h1>
       <p className='text-md text-gray-500'>Coming Soon!</p>
       <CallToAction />
     </div>
